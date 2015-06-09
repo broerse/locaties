e3a54c25d39fe8e52967eaebf9d11ddf014fe0cf
Martin Broerse <broerse@martinic.com>
2015-06-09 10:41:33 +0200

    typeForRoot has been renamed to modelNameFromPayloadKey

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,7 +1,7 @@
 import { Serializer } from 'ember-pouch';
 
 export default Serializer.extend({
-  typeForRoot: function(root) {
+  modelNameFromPayloadKey: function(root) {
     if (/ties$/i.test(root)) {
       return root.substr(0, root.length - 1);
     }
